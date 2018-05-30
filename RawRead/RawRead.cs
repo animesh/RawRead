dc017074cc0ce630d16ff034bb1216ee09c469f3
Animesh Sharma <animeshs@ntnu.no>
2018-05-30 20:25:01 +0200

    move to dotnet RawFileReader

--- RawRead/RawRead.cs
+++ RawRead/RawRead.cs
@@ -1,106 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
+﻿namespace RawRead{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.ExceptionServices;
+    using ThermoFisher.CommonCore.Data;
+    using ThermoFisher.CommonCore.Data.Business;
+    using ThermoFisher.CommonCore.Data.FilterEnums;
+    using ThermoFisher.CommonCore.Data.Interfaces;
+    using ThermoFisher.CommonCore.MassPrecisionEstimator;
+    using ThermoFisher.CommonCore.RawFileReader;  /* RawFileReader from Planet Orbitrap http://planetorbitrap.com/rawfilereader https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=jim.Shofstahl@thermofisher.com&su=Access%20to%20RawFileReader%20from%20Planet%20Orbitrap */
 
-using MSFileReaderLib;  /* add reference XRawfile2_x64.dll from 28.9 MB	MSFileReader2.2.zip available at https://thermo.flexnetoperations.com/control/thmo/login, then logging in and choosing "Utility Software".  */
-
-namespace RawRead
-{
-    class RawRead2PeakList
-    {
-        static void Main(string[] args)
-        {
-            MSFileReader_XRawfile rawMSfile = new MSFileReader_XRawfile();
-            rawMSfile.Open("F:\\promec\\Davi\\_QE\\BSAs\\20150512_BSA_The-PEG-envelope.raw");
-            rawMSfile.SetCurrentController(0, 1); /* Controller type 0 means mass spec device; Controller 1 means first MS device */
-            int nms = 0; /* ref int usrd with GetNumSpectra */
-            int pbMSData = 0;
-            int pnLastSpectrum = 0;
-            rawMSfile.GetNumSpectra(ref nms);
-            rawMSfile.IsThereMSData(ref pbMSData);
-            rawMSfile.GetLastSpectrumNumber(ref pnLastSpectrum);
-            Debug.WriteLine("Total Spectra: " + nms);
-            Debug.WriteLine("MSdata: " + pbMSData);
-            Debug.WriteLine("Last MSdata: " + pnLastSpectrum);
-            double pkWidCentroid = 0.0;
-            object mzList = null;
-            object pkFlg = null;
-            int arrLen = 0;
-            for (int i = 0; i < nms; i++)
-            {
-                rawMSfile.GetMassListFromScanNum(i, null, 1, 0, 0, 0, ref pkWidCentroid, ref mzList, ref pkFlg, ref arrLen);
-                double[,] mslist = (double[,])mzList;
-                double dMass = 0;
-                double dIntensity = 0;
-                for (long j = 0; j < arrLen; j++)
-                {
-                    dMass = mslist[0,j];
-                    dIntensity = mslist[1,j];
-                    Debug.WriteLine("Scan:" + i + "mass: " + dMass + "intensity: " + dIntensity);
+    class RawRead2PeakList{
+        static void Main(string[] args){
+            var rawFile = RawFileReaderAdapter.FileFactory(args[0]);
+            rawFile.SelectInstrument(Device.MS, 1);
+            int fms = rawFile.RunHeaderEx.FirstSpectrum;
+            int nms = rawFile.RunHeaderEx.LastSpectrum;
+            double sTime = rawFile.RunHeaderEx.StartTime;
+            double eTime = rawFile.RunHeaderEx.EndTime;
+            Console.WriteLine("#file:\t" + rawFile.FileName);
+            Console.WriteLine("#ms:\t" + nms);
+            Console.WriteLine("#RT:\t" + (eTime-sTime));
+            Console.WriteLine("#version:\t" + rawFile.FileHeader.Revision);
+            Console.WriteLine("#create date:\t" + rawFile.FileHeader.CreationDate);
+            Console.WriteLine("#machine:\t" + rawFile.FileHeader.WhoCreatedId);
+            Console.WriteLine("#serial#:\t" + rawFile.GetInstrumentData().SerialNumber);
+            Console.WriteLine("#writer:\t" + rawFile.GetInstrumentData().SoftwareVersion);
+            Console.WriteLine("#resolution\t:{0:F3}", rawFile.RunHeaderEx.MassResolution);
+            Console.WriteLine("#range:\t{0:F4}\t{1:F4}", rawFile.RunHeaderEx.LowMass, rawFile.RunHeaderEx.HighMass);
+            for (int i = fms; i < nms; i++){
+                var centroidStream = rawFile.GetCentroidStream(i, false);
+                Console.WriteLine("{0}\t{1}\t", i, centroidStream.Length);
+                for (int j = 0; j < centroidStream.Length; j++){
+                    Console.WriteLine("\t\t{0}\t{1:F4}\t{2:F0}\t{3:F0}", j, centroidStream.Masses[j], centroidStream.Intensities[j], centroidStream.Charges[j]);
                 }
-                Console.WriteLine("Scan:" + i + "MZ:" + arrLen + "mass: " + dMass + "intensity: " + dIntensity);
             }
         }
     }
 }
-
-/* http://bioinfo.kouwua.net/2012/09/read-thermo-raw-with-msfilereader-in-c.html
- * http://tools.thermofisher.com/content/sfs/manuals/Man-XCALI-97542-MSFileReader-30-Ref-ManXCALI97542-A-EN.pdf 
-  example for GetMassListFromScanNum
-typedef struct _datapeak
-{
-    double dMass;
-    double dIntensity;
-}
-DataPeak;
-long nScanNumber = 12; // read the contents of scan 12
-VARIANT varMassList;
-VariantInit(&varMassList);
-VARIANT varPeakFlags;
-VariantInit(&varPeakFlags);
-long nArraySize = 0;
-long nRet = XRawfileCtrl.GetMassListFromScanNum(&nScanNumber,
-NULL, 
-0, 
-0, 
-0,
-FALSE, 
-& varMassList, 
-& varPeakFlags,
-& nArraySize); 
-if( nRet != 0 )
-{
-::MessageBox(NULL, _T(“Error getting mass list data for scan 12.”), _T(“Error”), 
-MB_OK );
-}
-if( nArraySize )
-{
-SAFEARRAY FAR* psa = varMassList.parray;
-DataPeak* pDataPeaks = NULL;
-SafeArrayAccessData(psa, (void**)(&pDataPeaks) );
-for( long j = 0; j<nArraySize; j++ )
-{
-double dMass = pDataPeaks[j].dMass;
-double dIntensity = pDataPeaks[j].dIntensity;
-}
-SafeArrayUnaccessData(psa );
-}
-if( varMassList.vt != VT_EMPTY )
-{
-SAFEARRAY FAR* psa = varMassList.parray;
-varMassList.parray = NULL;
-// Delete the SafeArray
-SafeArrayDestroy(psa );
-}
-if(varPeakFlags.vt != VT_EMPTY )
-{
-SAFEARRAY FAR* psa = varPeakFlags.parray;
-varPeakFlags.parray = NULL;
-// Delete the SafeArray
-SafeArrayDestroy(psa );
-}
-*/
-
-
