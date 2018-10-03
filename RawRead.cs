9f8bf91f173fc3159e0c020e1c9f613723a9b4be
ani <sharma_animesh@hotmail.com>
2018-10-03 17:45:07 +0200

    record MSn ions as MGF collected in profile mode as well

--- RawRead.cs
+++ RawRead.cs
@@ -1,7 +1,7 @@
 ﻿//released under GPL version 2 or later: sharma.animesh@gmail.com
 //install mono and compile: mcs RawRead.cs /reference:ThermoFisher.CommonCore.RawFileReader.dll   /reference:ThermoFisher.CommonCore.Data.dll /reference:ThermoFisher.CommonCore.MassPrecisionEstimator.dll /reference:MathNet.Numerics.dll /reference:System.Numerics.dll
 //run: mono RawRead.exe <ThermoOrbitrapRawfileName> <intensityThreshold>(optional) <chargeThreshold>(optional)
-//windows csc.exe with dotnet
+//windows with dotnet: c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe RawRead.cs /reference:ThermoFisher.CommonCore.RawFileReader.dll   /reference:ThermoFisher.CommonCore.Data.dll /reference:ThermoFisher.CommonCore.MassPrecisionEstimator.dll /reference:MathNet.Numerics.dll /reference:System.Numerics.dll
 namespace RawRead
 {
     using ThermoFisher.CommonCore.RawFileReader;//RawFileReader from Planet Orbitrap http://planetorbitrap.com/rawfilereader
@@ -20,7 +20,8 @@ namespace RawRead
         {
             if (args.Length < 1 || !File.Exists(args[0])) { Console.WriteLine("USAGE: {0} fileName intensityThreshold(optional) chargeThreshold(optional)", AppDomain.CurrentDomain.FriendlyName); return; }
             var rawFile = RawFileReaderAdapter.FileFactory(args[0]);
-            if(!rawFile.IsOpen || rawFile.IsError) { Console.WriteLine("Error opening {1}, probably not proper orbitrap raw file? Tested only on Elite, QE and HF...", rawFile.FileError, args[0]); return; }
+            if(!rawFile.IsOpen) { Console.WriteLine("Raw file {1} is already Open, probably not finish writing", rawFile.FileError, args[0]); return; }
+            if(rawFile.IsError) { Console.WriteLine("Error opening {1}, probably not proper orbitrap raw file? Tested only on Elite, QE and HF...", rawFile.FileError, args[0]); return; }
             double insThr = 0;
             int chgThr = 0;
             if (args.Length == 2) { insThr = double.Parse(args[1]); }
@@ -35,7 +36,7 @@ namespace RawRead
             double eTime = rawFile.RunHeaderEx.EndTime;
             Console.WriteLine("#filename:\t" + rawFile.FileName + "\n" + "#prescan(s):\t" + nMS + "\n" + "#RT length:\t" + (eTime - sTime) + "\n" + "#version:\t" + rawFile.FileHeader.Revision + "\n" + "#create date:\t" + rawFile.FileHeader.CreationDate + "\n" + "#machine:\t" + rawFile.FileHeader.WhoCreatedId + "\n" + "#serial:\t" + rawFile.GetInstrumentData().SerialNumber + "\n" + "#writer:\t" + rawFile.GetInstrumentData().SoftwareVersion + "\n" + "#resolution:\t" + rawFile.RunHeaderEx.MassResolution + "\n" + "#massrange:\t" + rawFile.RunHeaderEx.LowMass + "-" + rawFile.RunHeaderEx.HighMass + "\n" + "#sample:\t" + rawFile.SampleInformation.Vial + "\n" + "#volume:\t" + rawFile.SampleInformation.SampleVolume + "\n" + "#injection:\t" + rawFile.SampleInformation.InjectionVolume + "\n" + "#dilution:\t" + rawFile.SampleInformation.DilutionFactor + "\n" + "#filter:\t" + fFilter.ToString() + "\n" + "#filterN:\t" + nFilter.ToString());
             //foreach (var device in rawFile.GetAllInstrumentNamesFromInstrumentMethod()) { Console.WriteLine("#method:\t" + device);}//windows only
-            StreamWriter writeMZ = new StreamWriter(rawFile.FileName + ".intensity" + insThr + ".charge" + chgThr + "." + "MZ.txt");
+            StreamWriter writeMZ = new StreamWriter(rawFile.FileName + ".MZ.txt");
             var scan = Scan.FromFile(rawFile, fMS);
             var scanEvent = rawFile.GetScanEventForScanNumber(fMS);
             LogEntry logEntry = rawFile.GetTrailerExtraInformation(fMS);
@@ -54,7 +55,7 @@ namespace RawRead
             ChromatogramTraceSettings settings = new ChromatogramTraceSettings(TraceType.BasePeak);
             var data = rawFile.GetChromatogramData(new IChromatogramSettings[] { settings }, fMS, nMS);
             var trace = ChromatogramSignal.FromChromatogramData(data);
-            StreamWriter writeChromatogram = new StreamWriter(rawFile.FileName + ".intensity" + insThr + ".charge" + chgThr + "." + "chromatogram.txt");
+            StreamWriter writeChromatogram = new StreamWriter(rawFile.FileName + ".chromatogram.txt");
             if (trace[0].Length > 0)
             {
                 writeChromatogram.WriteLine("BasePeak({0}points)\tRT\tIntensity", trace[0].Length);
@@ -64,8 +65,9 @@ namespace RawRead
             double tic = 0;
             double maxIntSum = 0;
             Complex[] samples = new Complex[nMS];
-            StreamWriter writeMS1 = new StreamWriter(rawFile.FileName + ".intensity" + insThr + ".charge" + chgThr + "." + "MS.txt");
-            StreamWriter writeMS2 = new StreamWriter(rawFile.FileName + ".intensity" + insThr + ".charge" + chgThr + "." + "MGF");
+            StreamWriter writeMS1 = new StreamWriter(rawFile.FileName + ".profile.intensity" + insThr + ".charge" + chgThr + "." + "MS.txt");
+            StreamWriter writeMS2 = new StreamWriter(rawFile.FileName + ".centroid." + "MGF");
+            StreamWriter writeMS2p = new StreamWriter(rawFile.FileName + ".profile." + "MGF");
             for (int i = fMS; i <= nMS; i++)
             {
                 double time = rawFile.RetentionTimeFromScanNumber(i);
@@ -75,20 +77,28 @@ namespace RawRead
                 var centroidStream = rawFile.GetCentroidStream(i, false);
                 double maxMass = 0;
                 double maxInt = 0;
-                string charge = "2";
-                //double charge = centroidStream.Charges[i];
-                if (scanStatistics.IsCentroidScan) // MS2
+                string charge = "";//centroidStream.Charges[i].ToString();
+                logEntry = rawFile.GetTrailerExtraInformation(i);
+                for (var l = 0; l < logEntry.Length; l++) {
+                  if(logEntry.Labels[l]=="Charge State:"){charge=logEntry.Values[l];}
+                  //Console.WriteLine("{0}-{1}-{2}",l,logEntry.Labels[l],logEntry.Values[l]);
+                }
+                if (scanStatistics.IsCentroidScan)
                 {
-                    //var trailerFields = rawFile.GetTrailerExtraHeaderInformation();
-                    //var scanTrailer = rawFile.GetTrailerExtraInformation(i);
-                    if(rawFile.GetInstrumentData().SerialNumber.Contains("Exactive")){charge = rawFile.GetTrailerExtraInformation(i).Values[7];}
-                    else{charge = rawFile.GetTrailerExtraInformation(i).Values[6];}
                     writeMS2.WriteLine("BEGIN IONS\nTITLE={0}\t{3}\tSCANS={2}\nRTINSECONDS={1}\nPEPMASS={6}\t{4}\t{5}\nCHARGE={7}+", i, time * 60, title, segmentedScan.Positions.Length, scanStatistics.BasePeakMass, scanStatistics.BasePeakIntensity, rawFile.GetScanEventForScanNumber(i).GetReaction(0).PrecursorMass,charge);
                     for (int j = 0; j < segmentedScan.Positions.Length; j++) { writeMS2.WriteLine("{0} {1}", segmentedScan.Positions[j], segmentedScan.Intensities[j]); }
                     writeMS2.WriteLine("END IONS\n");
                 }
-                else
+                else //profile?
                 {
+                  if (!title.Contains(" ms "))
+                  {
+                    writeMS2p.WriteLine("BEGIN IONS\nTITLE={0}\t{1}\tSCANS={2}\nRTINSECONDS={3}\nPEPMASS={4}\nCHARGE={5}+", i, title, centroidStream.Length, time * 60,  rawFile.GetScanEventForScanNumber(i).GetReaction(0).PrecursorMass,charge);
+                    for (int j = 0; j < centroidStream.Length; j++) { writeMS2p.WriteLine("{0} {1}", centroidStream.Masses[j], centroidStream.Intensities[j]); }
+                    writeMS2p.WriteLine("END IONS\n");
+                  }
+                  else //profile?
+                  {
                     writeMS1.WriteLine("Scan{0}\tMZ\tcharge\tintensity\t{1}\t{2}", i, title, centroidStream.Length);
                     for (int j = 0; j < centroidStream.Length; j++)
                     {
@@ -100,17 +110,19 @@ namespace RawRead
                         }
                         if (centroidStream.Intensities[j] >= maxInt) { maxInt = centroidStream.Intensities[j]; maxMass = centroidStream.Masses[j]; maxIntSum += maxInt; }
                     }
+                  }
                 }
                 Console.WriteLine("{0}\t{8}\t{7}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}", i, maxIntSum, title, maxMass, time, maxInt, charge, scanStatistics.TIC, scanStatistics.BasePeakMass);
-                samples[i-1] = new Complex(scanStatistics.BasePeakMass, scanStatistics.BasePeakIntensity);
+                samples[i-1] = new Complex(maxMass,maxInt);
             }
             Fourier.Forward(samples, FourierOptions.NoScaling);
             StreamWriter writeFFT = new StreamWriter(rawFile.FileName + ".intensity" + insThr + ".charge" + chgThr + "." + "FFT.txt");
-            for (int i = fMS; i < nMS; i++)
+            writeFFT.WriteLine("mass\tintensity\tangle\tmagnitude");
+            for (int i = fMS-1; i < nMS; i++)
             {
                 double magnitude = (2.0 / nMS) * (Math.Abs(Math.Sqrt(Math.Pow(samples[i].Real, 2) + Math.Pow(samples[i].Imaginary, 2))));
                 double angle = Math.Atan(samples[i].Imaginary / samples[i].Real);
-                writeFFT.WriteLine("{0}\t{1}\t{2}", i, magnitude, angle);
+                writeFFT.WriteLine("{0}\t{1}\t{2}\t{3}", samples[i].Real, samples[i].Imaginary, angle, magnitude);
             }
             Console.WriteLine("#TIC>={0}intensity:\t{1}", insThr, tic);
             Console.WriteLine("#Ions>=charge{0}:\t{1}", chgThr, tms);
--- Readme.md
+++ Readme.md
@@ -1,24 +1,39 @@
+
 # Prerequisites
 * [Mono](http://www.mono-project.com/download/stable/#download-lin) 
 * RawFileReader from [Planet Orbitrap](http://planetorbitrap.com/rawfilereader) or [email](https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=jim.Shofstahl@thermofisher.com&su=Access%20to%20RawFileReader%20from%20Planet%20Orbitrap)  jim.shofstahl@thermofisher.com with Subject "Access to RawFileReader"
 
 ## Compile
+
+### Linux
+
 ```bash
 mcs RawRead.cs /reference:ThermoFisher.CommonCore.RawFileReader.dll   /reference:ThermoFisher.CommonCore.Data.dll /reference:ThermoFisher.CommonCore.MassPrecisionEstimator.dll /reference:MathNet.Numerics.dll /reference:System.Numerics.dll
 ```
-###windows
-####c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe RawRead.cs /reference:ThermoFisher.CommonCore.RawFileReader.dll   /reference:ThermoFisher.CommonCore.Data.dll /reference:ThermoFisher.CommonCore.MassPrecisionEstimator.dll /reference:MathNet.Numerics.dll /reference:System.Numerics.dll
+
+
+#### Windows can also use dotnet cmd "csc"
+
+```bash
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe RawRead.cs /reference:ThermoFisher.CommonCore.RawFileReader.dll   /reference:ThermoFisher.CommonCore.Data.dll /reference:ThermoFisher.CommonCore.MassPrecisionEstimator.dll /reference:MathNet.Numerics.dll /reference:System.Numerics.dll
+```
 
 ## Usage
 ```bash
 mono RawRead.exe <ThermoOrbitrapRawfileName> <intensityThreshold>(optional) <chargeThreshold>(optional)
 ```
 
+##### Windows compiled via dotnet call directly
+```bash
+RawRead.exe <ThermoOrbitrapRawfileName> <intensityThreshold>(optional) <chargeThreshold>(optional)
+```
+
+
 ### Example
 * mono RawRead.exe 171010_Ip_Hela_ugi.raw (for all scans)
-* mono RawRead.exe <... rawFile> 0 2 (for scans with charge state > 1)
-* mono RawRead.exe <... rawFile> 10e6(for scans with recorded intensity > 1 million)
-* mono RawRead.exe <... rawFile> 10e6 2 (for scans with charge state > 1 AND recorded intensity > 1 million)
+* mono RawRead.exe <... rawFile> 0 2 (for profile scans with charge state > 1)
+* mono RawRead.exe <... rawFile> 10e6(for profile scans with recorded intensity > 1 million)
+* mono RawRead.exe <... rawFile> 10e6 2 (for profile scans with charge state > 1 AND recorded intensity > 1 million)
 
 ... 
 
@@ -30,7 +45,8 @@ mono RawRead.exe <ThermoOrbitrapRawfileName> <intensityThreshold>(optional) <cha
 ./comet.2018012.linux.exe 171010_Ip_Hela_ugi.raw.intensity0.charge0.MGF
 ```
 
-#### Search Raw file directly since [MaxQuant goes Linux](https://www.nature.com/articles/s41592-018-0018-y), we can finally perform a direct search :) though there is an annoying issues of reproducibility in comparison with Windows run. It differs by ~1% which (has been reported to developers)[https://maxquant.myjetbrains.com/youtrack/issue/MaxQuant-185] and there seems to be an inherent problem in the way mono handles numbers c.f. dotnet , anyways 1% is something one can live with ;)
+###  Search Raw file directly in linux
+since [MaxQuant goes Linux](https://www.nature.com/articles/s41592-018-0018-y), we can finally perform a direct search :) though there is an annoying issues of reproducibility in comparison with Windows run. It differs by ~1% which (has been reported to developers)[https://maxquant.myjetbrains.com/youtrack/issue/MaxQuant-185] and there seems to be an inherent problem in the way mono handles numbers c.f. dotnet , anyways 1% is something one can live with ;)
 
 * Download and Install [MaxQuant](http://www.coxdocs.org/doku.php?id=maxquant:common:download_and_installation)
 * create a parameter file for all raw files in the directory using the provided generic parameter file mqparTest.xml
