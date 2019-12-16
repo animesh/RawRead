85f05b8a1cddd3593387192a85d4a872872bd6e9
ani <sharma_animesh@hotmail.com>
2019-12-16 18:07:59 +0100

    change raw file to BSA, look at the MS1 profile and pick ions>50e5 intensity

--- RawRead.cs
+++ RawRead.cs
@@ -10,19 +10,17 @@ namespace RawRead
     using System.Numerics;//https://www.nuget.org/packages/System.Runtime.Numerics/
     using System;
     using System.IO;
-    using System.Collections.Generic;
-    using ThermoFisher.CommonCore.Data.Interfaces;
 
     internal class RawRead2PeakList
     {
         static void Main(string[] args)
         {
-            var rawFile = RawFileReaderAdapter.FileFactory("171010_Ip_Hela_ugi.raw");
+            var rawFile = RawFileReaderAdapter.FileFactory("20150512_BSA_The-PEG-envelope.raw");
             if (args.Length < 1 || !File.Exists(args[0])) { Console.WriteLine("USAGE: {0} fileName intensityThreshold(optional) chargeThreshold(optional), using default 171010_Ip_Hela_ugi.raw", AppDomain.CurrentDomain.FriendlyName);  }
             if(args.Length > 0){ rawFile = RawFileReaderAdapter.FileFactory(args[0]);}
             if(!rawFile.IsOpen) { Console.WriteLine("Raw file {1} is already Open, probably not finish writing", rawFile.FileError, args[0]); return; }
             if(rawFile.IsError) { Console.WriteLine("Error opening {1}, probably not proper orbitrap raw file? Tested only on Elite, QE and HF...", rawFile.FileError, args[0]); return; }
-            double insThr = 0;
+            double insThr = 50e5;
             if (args.Length == 2) { insThr = double.Parse(args[1]); }
             rawFile.SelectInstrument(Device.MS, 1);
             int fMS = rawFile.RunHeaderEx.FirstSpectrum;
@@ -38,25 +36,25 @@ namespace RawRead
             double maxIntSum = 0;
             Complex[] samples = new Complex[nMS];
             StreamWriter writeMS1 = new StreamWriter(rawFile.FileName + ".profile.intensity" + insThr +  ".MS.txt");
-            writeMS1.WriteLine("Scan\tBasePeak\tRT\tMaxIntensity\tCumulativeIntensity");
+            //writeMS1.WriteLine("Scan\tBasePeak\tRT\tMaxIntensity\tCumulativeIntensity");
             for (int i = fMS; i <= nMS; i++)
             {
                 double time = rawFile.RetentionTimeFromScanNumber(i);
                 string title = string.Join(Environment.NewLine, rawFile.GetScanEventForScanNumber(i));
                 var scanStatistics = rawFile.GetScanStatsForScanNumber(i);
-                var segmentedScan = rawFile.GetSegmentedScanFromScanNumber(i, scanStatistics);
                 var centroidStream = rawFile.GetCentroidStream(i, false);
-                double maxMass = 0;
                 double maxInt = 0;
-                if (title.Contains(" ms ") && scanStatistics.BasePeakIntensity>insThr){
+                if (title.Contains(" ms ") && scanStatistics.BasePeakIntensity>insThr)
+                {
                     for (int j = 0; j < centroidStream.Length; j++)
                     {
                         tic += centroidStream.Intensities[j];
                         tms++;
-                        if (centroidStream.Intensities[j] >= maxInt) { maxInt = centroidStream.Intensities[j]; maxMass = centroidStream.Masses[j]; maxIntSum += maxInt; }
+                        if (centroidStream.Intensities[j] >= maxInt) { maxInt = centroidStream.Intensities[j]; maxIntSum += maxInt; }
+                        writeMS1.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}", tms, time, i, j, centroidStream.Masses[j], centroidStream.Intensities[j], scanStatistics.BasePeakMass, maxInt, tic, maxIntSum);
                     }
-                writeMS1.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", i, scanStatistics.BasePeakMass, time,maxInt,maxIntSum);
-                samples[i-1] = new Complex(scanStatistics.BasePeakMass,time);
+                    //writeMS1.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", i, scanStatistics.BasePeakMass, time,maxInt,maxIntSum);
+                    samples[i-1] = new Complex(scanStatistics.BasePeakMass,time);
                 if(i>1&&Math.Ceiling(samples[i-1].Real)<samples[i-2].Real){Console.WriteLine("{0}\t{1}\t{2}\t{3}", i, scanStatistics.BasePeakMass, maxIntSum,samples[i-2].Real-samples[i-1].Real);}
                 }
             }
